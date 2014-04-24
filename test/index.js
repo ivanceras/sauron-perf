abfb0efc7174d8cd81a42979ca4b6c947e9f0a20
Matt Esch <matt@mattesch.info>
2014-04-24 09:05:29 +0100

    remove test dir

--- test/index.js
+++
@@ -1,8 +0,0 @@
-var test = require("tape")
-
-var mercuryPerf = require("../index")
-
-test("mercuryPerf is a function", function (assert) {
-    assert.equal(typeof mercuryPerf, "function")
-    assert.end()
-})
