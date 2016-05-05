5a3c40aa6dd37bc7909a0fdd86525e5bcc70435c
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2016-05-05 12:57:35 +0200

    showing react version in testsuite

--- resources/tests.js
+++ resources/tests.js
@@ -130,7 +130,7 @@ Suites.push({
 Suites.push({
     name: 'React',
     url: 'todomvc/react/index.html',
-    version: '0.14.8',
+    version: '0.15.1',
     prepare: function (runner, contentWindow, contentDocument) {
         contentWindow.Utils.store = function () {}
         return runner.waitForElement('.new-todo').then(function (element) {
