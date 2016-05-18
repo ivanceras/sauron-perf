cebd17a1fded1e74b878085a15502cfe9b5cfd1f
katopz <katopz@gmail.com>
2016-05-18 16:49:42 +0700

    Update knockout.

--- todomvc/knockoutjs/js/app.js
+++ todomvc/knockoutjs/js/app.js
@@ -175,10 +175,11 @@
 
 		// internal computed observable that fires whenever anything changes in our todos
 		ko.computed(function () {
-			// store a clean copy to local storage, which also creates a dependency on the observableArray and all observables in each item
+			// store a clean copy to local storage, which also creates a dependency on
+			// the observableArray and all observables in each item
 			localStorage.setItem('todos-knockoutjs', ko.toJSON(this.todos));
 		}.bind(this)).extend({
-			throttle: 500
+			rateLimit: { timeout: 500, method: 'notifyWhenChangesStop' }
 		}); // save at most twice per second
 	};
 
